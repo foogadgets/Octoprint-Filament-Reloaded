410d79b25b1d4f32082109e81289fe6e48a1ee67
Niclas Forslund <foogadgets@gmail.com>
2020-11-01 23:37:40 +0100

    added foogadget-repo-link

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Connor Huffine"
 plugin_author_email = "chuffine@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/kontakt/Octoprint-Filament-Reloaded"
+plugin_url = "https://github.com/foogadgets/Octoprint-Filament-Reloaded"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
